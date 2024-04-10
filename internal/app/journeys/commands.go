a05f73624dc1a003883f65f854ce703785c9cad7
Juha Lundan <juha.lundan@gmail.com>
2024-04-10 23:05:58 +0300

    feat: implement the first version of cache

--- internal/app/journeys/commands.go
+++ internal/app/journeys/commands.go
@@ -1,13 +1,59 @@
 package journeys
 
 import (
+	"bytes"
+	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/gorilla/mux"
 	"github.com/jlundan/journeys-api/internal/app/journeys/context/tre"
 	"github.com/jlundan/journeys-api/internal/app/journeys/routes"
 	"github.com/spf13/cobra"
 	"log"
+	"net/http"
 	"os"
 )
+
+var mc *memcache.Client
+
+func init() {
+	mc = memcache.New("localhost:11211")
+}
+
+func cacheMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.String()
+
+		item, err := mc.Get(key)
+		if err == nil {
+			// Cache hit, send response
+			_, err = w.Write(item.Value)
+			if err != nil {
+				rw := NewResponseWriter(w)
+				next.ServeHTTP(rw, r)
+			}
+			return
+		}
+
+		rw := NewResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		// Cache the new response
+		_ = mc.Set(&memcache.Item{Key: key, Value: rw.body.Bytes()})
+	})
+}
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	body *bytes.Buffer
+}
+
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, new(bytes.Buffer)}
+}
+
+func (rw *ResponseWriter) Write(b []byte) (int, error) {
+	rw.body.Write(b)
+	return rw.ResponseWriter.Write(b)
+}
 
 func Run() {
 	_ = mainCommand.Execute()
@@ -33,6 +79,7 @@ var mainCommand = &cobra.Command{
 		}
 
 		r := mux.NewRouter()
+		r.Use(cacheMiddleware)
 		routes.InjectMunicipalityRoutes(r, ctx)
 		routes.InjectLineRoutes(r, ctx)
 		routes.InjectJourneyPatternRoutes(r, ctx)
