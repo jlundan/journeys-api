a137fdd497fb1f9de9401813b832b80c11bae4b0
Juha Lundan <juha.lundan@gmail.com>
2024-09-29 23:38:48 +0300

    reafctor(ggtfs): stop times
    Make the stop times follow parsing mechanisms introduced in Agencies

--- internal/pkg/ggtfs/stoptimes.go
+++ internal/pkg/ggtfs/stoptimes.go
@@ -2,152 +2,117 @@ package ggtfs
 
 import (
 	"encoding/csv"
-	"errors"
 	"fmt"
-	"strconv"
 )
 
+// StopTime struct with fields as strings and optional fields as string pointers.
 type StopTime struct {
-	TripId            string
-	ArrivalTime       string
-	DepartureTime     string
-	StopId            string
-	StopSequence      int
-	StopHeadSign      *string
-	PickupType        *int
-	DropOffType       *int
-	ContinuousPickup  *int
-	ContinuousDropOff *int
-	ShapeDistTraveled *float64
-	Timepoint         *int
-	lineNumber        int
+	TripId            string  // trip_id
+	ArrivalTime       string  // arrival_time
+	DepartureTime     string  // departure_time
+	StopId            string  // stop_id
+	StopSequence      string  // stop_sequence (string for consistency)
+	StopHeadSign      *string // stop_headsign (optional)
+	PickupType        *string // pickup_type (optional)
+	DropOffType       *string // drop_off_type (optional)
+	ContinuousPickup  *string // continuous_pickup (optional)
+	ContinuousDropOff *string // continuous_drop_off (optional)
+	ShapeDistTraveled *string // shape_dist_traveled (optional)
+	Timepoint         *string // timepoint (optional)
+	LineNumber        int     // Line number in the CSV file for error reporting
 }
 
-var validStopTimeHeaders = []string{"trip_id", "arrival_time", "departure_time", "stop_id", "location_group_id",
-	"location_id", "stop_sequence", "stop_headsign", "start_pickup_drop_off_window", "end_pickup_drop_off_window",
-	"pickup_type", "drop_off_type", "continuous_pickup", "continuous_drop_off", "shape_dist_traveled", "timepoint",
-	"pickup_booking_rule_id", "drop_off_booking_rule_id"}
+// ValidStopTimeHeaders defines the headers expected in the stop_times CSV file.
+var validStopTimeHeaders = []string{"trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence",
+	"stop_headsign", "pickup_type", "drop_off_type", "continuous_pickup", "continuous_drop_off",
+	"shape_dist_traveled", "timepoint"}
 
+// LoadStopTimes loads stop times from a CSV reader and returns them along with any errors.
 func LoadStopTimes(csvReader *csv.Reader) ([]*StopTime, []error) {
-	stopPoints := make([]*StopTime, 0)
-	errs := make([]error, 0)
+	entities, errs := loadEntities(csvReader, validStopTimeHeaders, CreateStopTime, StopTimesFileName)
 
-	headers, err := ReadHeaderRow(csvReader, validStopTimeHeaders)
-	if err != nil {
-		errs = append(errs, createFileError(StopTimesFileName, fmt.Sprintf("read error: %v", err.Error())))
-		return stopPoints, errs
-	}
-	if headers == nil {
-		return stopPoints, errs
+	stopTimes := make([]*StopTime, 0, len(entities))
+	for _, entity := range entities {
+		if stopTime, ok := entity.(*StopTime); ok {
+			stopTimes = append(stopTimes, stopTime)
+		}
 	}
 
-	index := 0
-	for {
-		row, err := ReadDataRow(csvReader)
-		if err != nil {
-			errs = append(errs, createFileError(StopTimesFileName, fmt.Sprintf("%v", err.Error())))
-			index++
-			continue
-		}
-		if row == nil {
-			break
-		}
-
-		rowErrs := make([]error, 0)
-		stopPoint := StopTime{
-			lineNumber: index,
-		}
-
-		var (
-			tripId        *string
-			stopId        *string
-			arrivalTime   *string
-			departureTime *string
-			sequence      *int
-		)
-		for name, column := range headers {
-			switch name {
-			case "trip_id":
-				tripId = handleIDField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "arrival_time":
-				arrivalTime = handleTimeField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "departure_time":
-				departureTime = handleTimeField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "stop_id":
-				stopId = handleIDField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "stop_sequence":
-				sequence = handleIntField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "stop_headsign":
-				stopPoint.StopHeadSign = &row[column]
-			case "pickup_type":
-				stopPoint.PickupType = handlePickupField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "drop_off_type":
-				stopPoint.DropOffType = handleDropOffField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "continuous_pickup":
-				stopPoint.ContinuousPickup = handleContinuousPickupField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "continuous_drop_off":
-				stopPoint.ContinuousDropOff = handleContinuousDropOffField(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "shape_dist_traveled":
-				stopPoint.ShapeDistTraveled = handleFloat64Field(row[column], StopTimesFileName, name, index, &rowErrs)
-			case "timepoint":
-				stopPoint.Timepoint = handleTimePointField(row[column], StopTimesFileName, name, index, &rowErrs)
-			}
-		}
-
-		if tripId == nil {
-			rowErrs = append(rowErrs, createFileRowError(StopTimesFileName, stopPoint.lineNumber, "trip_id must be specified"))
-		} else {
-			stopPoint.TripId = *tripId
-		}
-
-		if stopId == nil {
-			rowErrs = append(rowErrs, createFileRowError(StopTimesFileName, stopPoint.lineNumber, "stop_id must be specified"))
-		} else {
-			stopPoint.StopId = *stopId
-		}
-
-		if sequence == nil {
-			rowErrs = append(rowErrs, createFileRowError(StopTimesFileName, stopPoint.lineNumber, "stop_sequence must be specified"))
-		} else {
-			stopPoint.StopSequence = *sequence
-		}
-
-		if arrivalTime == nil {
-			rowErrs = append(rowErrs, createFileRowError(StopTimesFileName, stopPoint.lineNumber, "arrival_time must be specified"))
-		} else {
-			stopPoint.ArrivalTime = *arrivalTime
-		}
-
-		if departureTime == nil {
-			rowErrs = append(rowErrs, createFileRowError(StopTimesFileName, stopPoint.lineNumber, "departure_time must be specified"))
-		} else {
-			stopPoint.DepartureTime = *departureTime
-		}
-
-		if len(rowErrs) > 0 {
-			errs = append(errs, rowErrs...)
-		} else {
-			stopPoints = append(stopPoints, &stopPoint)
-		}
-
-		index++
-	}
-
-	return stopPoints, errs
+	return stopTimes, errs
 }
 
-func ValidateStoptimes(stopTimes []*StopTime, stops []*Stop) []error {
+// CreateStopTime creates and validates a StopTime instance from the CSV row data.
+func CreateStopTime(row []string, headers map[string]uint8, lineNumber int) (interface{}, []error) {
+	var parseErrors []error
+
+	stopTime := StopTime{
+		LineNumber: lineNumber,
+	}
+
+	for hName, hPos := range headers {
+		switch hName {
+		case "trip_id":
+			stopTime.TripId = getField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "arrival_time":
+			stopTime.ArrivalTime = getField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "departure_time":
+			stopTime.DepartureTime = getField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "stop_id":
+			stopTime.StopId = getField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "stop_sequence":
+			stopTime.StopSequence = getField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "stop_headsign":
+			stopTime.StopHeadSign = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "pickup_type":
+			stopTime.PickupType = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "drop_off_type":
+			stopTime.DropOffType = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "continuous_pickup":
+			stopTime.ContinuousPickup = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "continuous_drop_off":
+			stopTime.ContinuousDropOff = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "shape_dist_traveled":
+			stopTime.ShapeDistTraveled = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		case "timepoint":
+			stopTime.Timepoint = getOptionalField(row, hName, hPos, &parseErrors, lineNumber, StopTimesFileName)
+		}
+	}
+
+	if len(parseErrors) > 0 {
+		return &stopTime, parseErrors
+	}
+	return &stopTime, nil
+}
+
+// ValidateStopTimes performs additional validation for a list of StopTime instances.
+func ValidateStopTimes(stopTimes []*StopTime, stops []*Stop) []error {
 	var validationErrors []error
 
 	if stopTimes == nil {
 		return validationErrors
 	}
 
-	// Stop times are grouped by trip id, and we want to make sure that each trip id has at least two stops
-	// We do this by first finding out all trips in the stopTimes and setting their stop count to zero in a map. Then we loop
-	// through all stop times while incrementing the stop counts in our map. Eventually we iterate through the map and check
-	// if any trip has stop count less than a zero.
-	tripIdToStopCount := make(map[string]uint64)
+	for _, stopTime := range stopTimes {
+		// Additional required field checks for individual StopTime.
+		if stopTime.TripId == "" {
+			validationErrors = append(validationErrors, createFileRowError(StopTimesFileName, stopTime.LineNumber, "trip_id must be specified"))
+		}
+		if stopTime.ArrivalTime == "" {
+			validationErrors = append(validationErrors, createFileRowError(StopTimesFileName, stopTime.LineNumber, "arrival_time must be specified"))
+		}
+		if stopTime.DepartureTime == "" {
+			validationErrors = append(validationErrors, createFileRowError(StopTimesFileName, stopTime.LineNumber, "departure_time must be specified"))
+		}
+		if stopTime.StopId == "" {
+			validationErrors = append(validationErrors, createFileRowError(StopTimesFileName, stopTime.LineNumber, "stop_id must be specified"))
+		}
+		if stopTime.StopSequence == "" {
+			validationErrors = append(validationErrors, createFileRowError(StopTimesFileName, stopTime.LineNumber, "stop_sequence must be specified"))
+		}
+	}
+
+	// Group stop times by trip_id to ensure each trip has at least two stops.
+	tripIdToStopCount := make(map[string]int)
 	for _, stopTimeItem := range stopTimes {
 		if stopTimeItem == nil {
 			continue
@@ -185,49 +150,4 @@ func ValidateStoptimes(stopTimes []*StopTime, stops []*Stop) []error {
 	}
 
 	return validationErrors
-}
-
-func handlePickupField(str string, fileName string, fieldName string, index int, errs *[]error) *int {
-	n, err := strconv.ParseInt(str, 10, 64)
-	if err != nil {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, err))
-		return nil
-	}
-
-	if v := int(n); v <= 4 {
-		return &v
-	} else {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, errors.New(invalidValue)))
-		return nil
-	}
-}
-
-func handleDropOffField(str string, fileName string, fieldName string, index int, errs *[]error) *int {
-	n, err := strconv.ParseInt(str, 10, 64)
-	if err != nil {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, err))
-		return nil
-	}
-
-	if v := int(n); v <= 4 {
-		return &v
-	} else {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, errors.New(invalidValue)))
-		return nil
-	}
-}
-
-func handleTimePointField(str string, fileName string, fieldName string, index int, errs *[]error) *int {
-	n, err := strconv.ParseInt(str, 10, 64)
-	if err != nil {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, err))
-		return nil
-	}
-
-	if v := int(n); v <= 4 {
-		return &v
-	} else {
-		*errs = append(*errs, createFieldError(fileName, fieldName, index, errors.New(invalidValue)))
-		return nil
-	}
 }
