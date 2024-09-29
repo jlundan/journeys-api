cb06092fbe48efac517f3196977dc33b5f9eaa61
Juha Lundan <juha.lundan@gmail.com>
2024-09-29 23:12:32 +0300

    reafctor(ggtfs): stops
    Make the stops follow parsing mechanisms introduced in Agencies

--- internal/pkg/ggtfs/stops_test.go
+++ internal/pkg/ggtfs/stops_test.go
@@ -4,43 +4,57 @@ package ggtfs
 
 import (
 	"encoding/csv"
-	"encoding/json"
-	"fmt"
-	"sort"
 	"strings"
 	"testing"
 )
 
-func TestStopCSVParsing(t *testing.T) {
-	items, errors := LoadStops(csv.NewReader(strings.NewReader("")))
+func TestShouldReturnEmptyStopArrayOnEmptyString(t *testing.T) {
+	stops, errors := LoadStops(csv.NewReader(strings.NewReader("")))
 	if len(errors) > 0 {
 		t.Error(errors)
 	}
-	if len(items) != 0 {
-		t.Error("expected zero items")
-	}
-
-	reader := csv.NewReader(strings.NewReader("foo,bar\n1,2"))
-	reader.Comma = ','
-	reader.Comment = ','
-	_, errors = LoadStops(reader)
-	if len(errors) == 0 {
-		t.Error("expected to throw error")
+	if len(stops) != 0 {
+		t.Error("expected zero calendar items")
 	}
 }
 
-func TestStopParsingOK(t *testing.T) {
+func TestStopParsing(t *testing.T) {
+	loadStopsFunc := func(reader *csv.Reader) ([]interface{}, []error) {
+		stops, errs := LoadStops(reader)
+		entities := make([]interface{}, len(stops))
+		for i, stop := range stops {
+			entities[i] = stop
+		}
+		return entities, errs
+	}
+
+	validateStopsFunc := func(entities []interface{}) []error {
+		stops := make([]*Stop, len(entities))
+		for i, entity := range entities {
+			if stop, ok := entity.(*Stop); ok {
+				stops[i] = stop
+			}
+		}
+
+		return ValidateStops(stops)
+	}
+
+	runGenericGTFSParseTest(t, "StopOKTestcases", loadStopsFunc, validateStopsFunc, false, getStopOKTestcases())
+	runGenericGTFSParseTest(t, "StopNOKTestcases", loadStopsFunc, validateStopsFunc, false, getStopNOKTestcases())
+}
+
+func getStopOKTestcases() map[string]ggtfsTestCase {
 	code := "0001"
 	name := "Place 0001"
 	desc := "Stop at place 0001"
-	lat := 11.1111111
-	lon := -11.1111111
+	lat := "11.1111111"
+	lon := "-11.1111111"
 	zone := "Z1"
 	stopUrl := "https://acme.inc/stops/0001"
-	location := 0
+	location := "0"
 	parentStation := "4"
 	timeZone := "Europe/Helsinki"
-	wheelchairBoarding := 0
+	wheelchairBoarding := "0"
 	level := "1"
 	platformCode := "0001"
 
@@ -61,133 +75,65 @@ func TestStopParsingOK(t *testing.T) {
 		LevelId:            &level,
 	}
 
-	testCases := []struct {
-		headers  map[string]uint8
-		rows     [][]string
-		expected Stop
-	}{
-		{
-			rows: [][]string{
-				{"stop_id", "stop_code", "stop_name", "stop_desc", "stop_lat", "stop_lon",
-					"zone_id", "stop_url", "location_type", "parent_station", "stop_timezone", "wheelchair_boarding", "level_id", "platform_code"},
-				{"0001", "0001", "Place 0001", "Stop at place 0001", "11.1111111", "-11.1111111", "Z1",
-					"https://acme.inc/stops/0001", "0", "4", "Europe/Helsinki", "0", "1", "0001"},
-			},
-			expected: expected1,
+	testCases := make(map[string]ggtfsTestCase)
+	testCases["1"] = ggtfsTestCase{
+		csvRows: [][]string{
+			{"stop_id", "stop_code", "stop_name", "stop_desc", "stop_lat", "stop_lon",
+				"zone_id", "stop_url", "location_type", "parent_station", "stop_timezone", "wheelchair_boarding", "level_id", "platform_code"},
+			{"0001", "0001", "Place 0001", "Stop at place 0001", "11.1111111", "-11.1111111", "Z1",
+				"https://acme.inc/stops/0001", "0", "4", "Europe/Helsinki", "0", "1", "0001"},
 		},
+		expectedStructs: []interface{}{&expected1},
 	}
 
-	for _, tc := range testCases {
-		stops, err := LoadStops(csv.NewReader(strings.NewReader(tableToString(tc.rows))))
-		if err != nil && len(err) > 0 {
-			t.Error(err)
-			continue
-		}
-
-		if len(stops) != 1 {
-			t.Error("expected one row")
-			continue
-		}
-
-		if !stopsMatch(tc.expected, *stops[0]) {
-			s1, err := json.Marshal(tc.expected)
-			if err != nil {
-				t.Error(err)
-			}
-			s2, err := json.Marshal(*stops[0])
-			if err != nil {
-				t.Error(err)
-			}
-			t.Error(fmt.Sprintf("expected %v, got %v", string(s1), string(s2)))
-		}
-	}
+	return testCases
 }
 
-func TestStopParsingNOK(t *testing.T) {
-	testCases := []struct {
-		rows     [][]string
-		expected []string
-	}{
-		{
-			rows: [][]string{
-				{"stop_id", "stop_name", "stop_lat", "stop_lon", "parent_station"},
-				{","},
-				{"", "foo", "11.11", "22.22", "0002"},
-				{"0001", "foo", "11.11", "22.22", "0002"},
-				{"0001", "foo", "invalid", "22.22", "0002"},
-			},
-			expected: []string{
-				"stops.txt: record on line 2: wrong number of fields",
-				"stops.txt:1: stop_id: empty value not allowed",
-				"stops.txt:1: stop_id must be specified",
-				"stops.txt:3: non-unique id: stop_id",
-				"stops.txt:3: stop_lat: strconv.ParseFloat: parsing \"invalid\": invalid syntax",
-			},
+func getStopNOKTestcases() map[string]ggtfsTestCase {
+	testCases := make(map[string]ggtfsTestCase)
+	testCases["1"] = ggtfsTestCase{
+		csvRows: [][]string{
+			{"stop_id", "stop_name", "stop_lat", "stop_lon", "parent_station"},
+			{","},
+			{"", "foo", "11.11", "22.22", "0002"},
+			{"0001", "foo", "11.11", "22.22", "0002"},
+			{"0001", "foo", "invalid", "22.22", "0002"},
 		},
-		{
-			rows: [][]string{
-				{"stop_id", "location_type"},
-				{"0001", "2"},
-			},
-			expected: []string{
-				"stops.txt:0: stop_name must be specified for location types 0,1 and 2",
-				"stops.txt:0: stop_lat must be specified for location types 0,1 and 2",
-				"stops.txt:0: stop_lon must be specified for location types 0,1 and 2",
-				"stops.txt:0: parent_station must be specified for location types 2,3 and 4",
-			},
+		expectedErrors: []string{
+			"stops.txt: record on line 2: wrong number of fields",
+			"stops.txt:1: stop_id: empty value not allowed",
+			"stops.txt:1: stop_id must be specified",
+			"stops.txt:3: non-unique id: stop_id",
+			"stops.txt:3: stop_lat: strconv.ParseFloat: parsing \"invalid\": invalid syntax",
 		},
-		{
-			rows: [][]string{
-				{"stop_id", "stop_name", "stop_lat", "stop_lon", "parent_station", "location_type", "wheelchair_boarding"},
-				{"0000", "foo", "11.11", "22.22", "0002", "10", "4"},
-				{"0001", "foo", "11.11", "22.22", "0002", "4", "10"},
-				{"0002", "foo", "11.11", "22.22", "0002", "invalid", "4"},
-				{"0003", "foo", "11.11", "22.22", "0002", "4", "invalid"},
-			},
-			expected: []string{
-				"stops.txt:0: location_type: invalid value",
-				"stops.txt:1: wheelchair_boarding: invalid value",
-				"stops.txt:2: location_type: strconv.ParseInt: parsing \"invalid\": invalid syntax",
-				"stops.txt:3: wheelchair_boarding: strconv.ParseInt: parsing \"invalid\": invalid syntax",
-			},
+	}
+	testCases["2"] = ggtfsTestCase{
+		csvRows: [][]string{
+			{"stop_id", "location_type"},
+			{"0001", "2"},
+		},
+		expectedErrors: []string{
+			"stops.txt:0: stop_name must be specified for location types 0,1 and 2",
+			"stops.txt:0: stop_lat must be specified for location types 0,1 and 2",
+			"stops.txt:0: stop_lon must be specified for location types 0,1 and 2",
+			"stops.txt:0: parent_station must be specified for location types 2,3 and 4",
+		},
+	}
+	testCases["3"] = ggtfsTestCase{
+		csvRows: [][]string{
+			{"stop_id", "stop_name", "stop_lat", "stop_lon", "parent_station", "location_type", "wheelchair_boarding"},
+			{"0000", "foo", "11.11", "22.22", "0002", "10", "4"},
+			{"0001", "foo", "11.11", "22.22", "0002", "4", "10"},
+			{"0002", "foo", "11.11", "22.22", "0002", "invalid", "4"},
+			{"0003", "foo", "11.11", "22.22", "0002", "4", "invalid"},
+		},
+		expectedErrors: []string{
+			"stops.txt:0: location_type: invalid value",
+			"stops.txt:1: wheelchair_boarding: invalid value",
+			"stops.txt:2: location_type: strconv.ParseInt: parsing \"invalid\": invalid syntax",
+			"stops.txt:3: wheelchair_boarding: strconv.ParseInt: parsing \"invalid\": invalid syntax",
 		},
 	}
 
-	for _, tc := range testCases {
-		_, err := LoadStops(csv.NewReader(strings.NewReader(tableToString(tc.rows))))
-
-		sort.Slice(err, func(x, y int) bool {
-			return err[x].Error() < err[y].Error()
-		})
-
-		sort.Slice(tc.expected, func(x, y int) bool {
-			return tc.expected[x] < tc.expected[y]
-		})
-
-		if len(err) == 0 {
-			t.Error("expected to throw an error")
-			continue
-		}
-
-		if len(err) != len(tc.expected) {
-			t.Error(fmt.Sprintf("expected %v errors, got %v", len(tc.expected), len(err)))
-			for _, e := range err {
-				fmt.Println(e)
-			}
-			continue
-		}
-
-		for i, e := range err {
-			if e.Error() != tc.expected[i] {
-				t.Error(fmt.Sprintf("expected error %s, got %s", tc.expected[i], e.Error()))
-			}
-		}
-	}
-}
-
-func stopsMatch(a Stop, b Stop) bool {
-	return a.Id == b.Id && *a.Code == *b.Code && *a.Name == *b.Name && *a.Desc == *b.Desc && *a.Lat == *b.Lat &&
-		*a.Lon == *b.Lon && *a.ZoneId == *b.ZoneId && *a.Url == *b.Url && *a.LocationType == *b.LocationType &&
-		*a.ParentStation == *b.ParentStation && *a.Timezone == *b.Timezone && *a.WheelchairBoarding == *b.WheelchairBoarding &&
-		*a.PlatformCode == *b.PlatformCode && *a.LevelId == *b.LevelId
+	return testCases
 }
