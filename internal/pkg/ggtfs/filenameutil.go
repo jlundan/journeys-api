0fe140471c11891b559435ca39585288b0517f03
Juha Lundan <juha.lundan@gmail.com>
2024-12-29 20:45:15 +0200

    refactor(ggtfs): refactor files
    Try to move away from generic terms such as "common" and split utils into more descriptive files.

---
+++ internal/pkg/ggtfs/filenameutil.go
@@ -0,0 +1,24 @@
+package ggtfs
+
+//goland:noinspection GoUnusedConst
+const (
+	AgenciesFileName      = "agency.txt"
+	RoutesFileName        = "routes.txt"
+	StopsFileName         = "stops.txt"
+	TripsFileName         = "trips.txt"
+	StopTimesFileName     = "stop_times.txt"
+	CalendarFileName      = "calendar.txt"
+	CalendarDatesFileName = "calendar_dates.txt"
+	ShapesFileName        = "shapes.txt"
+	//These are not used yet, but are part of the spec
+	//FareAttributesFileName = "fare_attributes.txt"
+	//FareRulesFileName      = "fare_rules.txt"
+	//FrequenciesFileName    = "frequencies.txt"
+	//TransfersFileName      = "transfers.txt"
+	//PathwaysFileName       = "pathways.txt"
+	//LevelsFileName         = "levels.txt"
+
+	emptyValueNotAllowed = "empty value not allowed"
+	invalidValue         = "invalid value"
+	nonUniqueId          = "non-unique id"
+)
