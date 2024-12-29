a895b9c52af3ffe5b98fd8deaa0916402604d7ac
Juha Lundan <juha.lundan@gmail.com>
2024-12-29 06:57:22 +0200

    refactor(ggtfs): field validation logic + stop times

--- internal/pkg/ggtfs/common.go
+++ internal/pkg/ggtfs/common.go
@@ -100,6 +100,28 @@ type ValidAndPresentField interface {
 	IsPresent() bool
 }
 
+type FieldTobeValidated interface {
+	IsValid() bool
+	IsPresent() bool
+	IsEmpty() bool
+}
+
+func validateRequiredFields(fields map[string]FieldTobeValidated, validationErrors *[]error, lineNumber int, fileName string) {
+	for name, value := range fields {
+		if !value.IsValid() {
+			*validationErrors = append(*validationErrors, createFileRowError(fileName, lineNumber, createInvalidRequiredFieldString(name)))
+		}
+	}
+}
+
+func validateOptionalFields(fields map[string]FieldTobeValidated, validationErrors *[]error, lineNumber int, fileName string) {
+	for name, value := range fields {
+		if value.IsPresent() && !value.IsEmpty() && !value.IsValid() {
+			*validationErrors = append(*validationErrors, createFileRowError(fileName, lineNumber, createInvalidFieldString(name)))
+		}
+	}
+}
+
 func getRowValue(row []string, position int) *string {
 	if position < 0 || position >= len(row) {
 		return nil
