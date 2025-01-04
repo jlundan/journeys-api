15bee4cfdf6d1468f80b34a2de4c5d384268f807
Juha Lundan <juha.lundan@gmail.com>
2025-01-04 15:09:08 +0200

    refactor: server startup, middleware and caching

---
+++ internal/app/journeys/memcached.go
@@ -0,0 +1,57 @@
+package journeys
+
+import (
+	"bytes"
+	"errors"
+	"github.com/bradfitz/gomemcache/memcache"
+	"net/http"
+	"os"
+)
+
+func NewMemcachedCacheMiddleware(client *memcache.Client) (*MemcachedCacheMiddleware, error) {
+	if os.Getenv("MEMCACHED_URL") == "" {
+		return nil, errors.New("MEMCACHED_URL not set in environment, but memcached is configured. Cannot proceed")
+	}
+	return &MemcachedCacheMiddleware{client: client}, nil
+}
+
+type MemcachedCacheMiddleware struct {
+	client *memcache.Client
+}
+
+func (mcm *MemcachedCacheMiddleware) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.String()
+
+		item, err := mcm.client.Get(key)
+		if err == nil {
+			// Cache hit, send response
+			_, err = w.Write(item.Value)
+			if err != nil {
+				rw := NewResponseWriter(w)
+				next.ServeHTTP(rw, r)
+			}
+			return
+		}
+
+		rw := NewResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		// Cache the new response
+		_ = mcm.client.Set(&memcache.Item{Key: key, Value: rw.forCache.Bytes()})
+	})
+}
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	forCache *bytes.Buffer
+}
+
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, new(bytes.Buffer)}
+}
+
+func (rw *ResponseWriter) Write(b []byte) (int, error) {
+	rw.forCache.Write(b)
+	return rw.ResponseWriter.Write(b)
+}
