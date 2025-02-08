98a112719acc2b4717e1cfb71e7f78c0c3caa19f
Juha Lundan <juha.lundan@gmail.com>
2025-02-08 18:24:23 +0200

    fix: handle nil checks and trims for stop-point fields

--- internal/app/journeys/repository/stops.go
+++ internal/app/journeys/repository/stops.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"github.com/jlundan/journeys-api/internal/app/journeys/model"
 	"github.com/jlundan/journeys-api/pkg/ggtfs"
+	"log"
 	"math"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 func newStopPointsRepository(stops []*ggtfs.Stop, municipalityDataStore *JourneysMunicipalitiesRepository) *JourneysStopPointsRepository {
@@ -14,26 +16,53 @@ func newStopPointsRepository(stops []*ggtfs.Stop, municipalityDataStore *Journey
 	var byId = make(map[string]*model.StopPoint)
 
 	for _, stop := range stops {
-
-		// FIXME: Nil-checks
-		lat, err := strconv.ParseFloat(*stop.Lat, 64)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("Error parsing shape.PtLat: %v, line: %v", *stop.Lat, stop.LineNumber))
+		var lat, lon float64
+		if stop.Lat != nil {
+			lf, err := strconv.ParseFloat(*stop.Lat, 64)
+			if err != nil {
+				log.Println(fmt.Sprintf("stop-point (on gtfs line %v): cannot parse lat float value", stop.LineNumber))
+			}
+			lat = lf
+		} else {
+			log.Println(fmt.Sprintf("stop-point (on gtfs line %v): lat is missing", stop.LineNumber))
 		}
-		lon, err := strconv.ParseFloat(*stop.Lon, 64)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("Error parsing shape.PtLon: %v, line: %v", *stop.Lon, stop.LineNumber))
+
+		if stop.Lon != nil {
+			lf, err := strconv.ParseFloat(*stop.Lon, 64)
+			if err != nil {
+				log.Println(fmt.Sprintf("stop-point (on gtfs line %v): cannot parse lon float value", stop.LineNumber))
+			}
+			lon = lf
+		} else {
+			log.Println(fmt.Sprintf("stop-point (on gtfs line %v): lon is missing", stop.LineNumber))
 		}
 
-		lat2 := math.Round(lat*100000) / 100000
-		lon2 := math.Round(lon*100000) / 100000
+		var name, shortName, tariffZone string
+
+		if stop.Name != nil {
+			name = strings.TrimSpace(*stop.Name)
+		} else {
+			log.Println(fmt.Sprintf("stop-point (on gtfs line %v): name is missing", stop.LineNumber))
+		}
+
+		if stop.Code != nil {
+			shortName = strings.TrimSpace(*stop.Code)
+		} else {
+			log.Println(fmt.Sprintf("stop-point (on gtfs line %v): shortName is missing", stop.LineNumber))
+		}
+
+		if stop.ZoneId != nil {
+			tariffZone = strings.TrimSpace(*stop.ZoneId)
+		} else {
+			log.Println(fmt.Sprintf("stop-point (on gtfs line %v): tariffZone is missing", stop.LineNumber))
+		}
 
 		s := model.StopPoint{
-			Name:       *stop.Name,
-			ShortName:  *stop.Code,
-			Latitude:   lat2,
-			Longitude:  lon2,
-			TariffZone: *stop.ZoneId,
+			Name:       name,
+			ShortName:  shortName,
+			Latitude:   math.Round(lat*100000) / 100000,
+			Longitude:  math.Round(lon*100000) / 100000,
+			TariffZone: tariffZone,
 		}
 
 		if !ggtfs.StringIsNilOrEmpty(stop.Extensions.MunicipalityId) {
