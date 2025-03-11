7ee81816a821368c504335a5bfe26da6ca22ba74
Juha Lundan <juha.lundan@gmail.com>
2025-03-11 17:29:40 +0200

    fix: implement cache controls
    This commit implements cache controls which allow the cached items to expire according to configured rules. Two cache modes are supported: short and long. The short cache can be used for example during night when the service day is about to expire, and the cache must invalidate during the period between last journey of the previous service day and the start of the next service day.

--- internal/app/journeys/server/memcached.go
+++ internal/app/journeys/server/memcached.go
@@ -6,17 +6,36 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 	"net/http"
 	"os"
+	"time"
 )
 
-func NewMemcachedCacheMiddleware(client *memcache.Client) (*MemcachedCacheMiddleware, error) {
+func NewMemcachedCacheMiddleware(client *memcache.Client, shortCacheDuration time.Duration, longCacheDuration time.Duration, shortCachePeriodLowerBound int, shortCachePeriodUpperBound int) (*MemcachedCacheMiddleware, error) {
 	if os.Getenv("MEMCACHED_URL") == "" {
 		return nil, errors.New("MEMCACHED_URL not set in environment, but memcached is configured. Cannot proceed")
 	}
-	return &MemcachedCacheMiddleware{client: client}, nil
+	return &MemcachedCacheMiddleware{
+		client:                     client,
+		shortCacheDuration:         shortCacheDuration,
+		longCacheDuration:          longCacheDuration,
+		shortCachePeriodLowerBound: shortCachePeriodLowerBound,
+		shortCachePeriodUpperBound: shortCachePeriodUpperBound,
+	}, nil
 }
 
 type MemcachedCacheMiddleware struct {
-	client *memcache.Client
+	client                     *memcache.Client
+	shortCacheDuration         time.Duration
+	longCacheDuration          time.Duration
+	shortCachePeriodLowerBound int
+	shortCachePeriodUpperBound int
+}
+
+func (mcm *MemcachedCacheMiddleware) Flush() error {
+	err := mcm.client.DeleteAll()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (mcm *MemcachedCacheMiddleware) Middleware(next http.Handler) http.Handler {
@@ -37,8 +56,23 @@ func (mcm *MemcachedCacheMiddleware) Middleware(next http.Handler) http.Handler 
 		rw := NewResponseWriter(w)
 		next.ServeHTTP(rw, r)
 
+		// Determine expiration based on time of day
+		// Night hours (e.g., 00:00 - 05:00) have a shorter cache duration
+		// This is because the "service day" ends at night after the last service has completed. There is often
+		// a gap between the last service and the first service of the next day, which allows the cache to clear before
+		// the next day's services begin if the cache period is short.
+		var expiration int32
+		now := time.Now()
+		hour := now.Hour()
+
+		if hour >= mcm.shortCachePeriodLowerBound && hour <= mcm.shortCachePeriodUpperBound { // Night hours (e.g., 00:00 - 05:00)
+			expiration = int32(time.Now().Add(mcm.shortCacheDuration).Unix())
+		} else {
+			expiration = int32(time.Now().Add(mcm.longCacheDuration).Unix())
+		}
+
 		// Cache the new response
-		_ = mcm.client.Set(&memcache.Item{Key: key, Value: rw.forCache.Bytes()})
+		_ = mcm.client.Set(&memcache.Item{Key: key, Value: rw.forCache.Bytes(), Expiration: expiration})
 	})
 }
 
